226a976cce83b92eb25383d118766b03fa440cac
Bodikov Kuat <kuat.bodikov@gmail.com>
2022-03-01 15:30:52 +0500

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Kuat Bodikov on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
